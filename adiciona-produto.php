69ad0cc8fc71c4a4d0a3f3b1e7ca1ee44768ea3e
thiagobergami <thiagobergamig@gmail.com>
2016-08-11 15:38:32 -0300

    Fix error in usado

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -6,7 +6,12 @@
     $preco = $_POST["preco"];
     $descricao = $_POST['descricao'];
     $categoria_id = $_POST['categoria_id'];
-    $usado = $_POST['usado'];
+    if(array_key_exists('usado', $_POST)){
+        $usado = true;
+    }else{
+        $usado = false;
+    }
+
 
     if(insereProduto($conexao,$nome, $preco, $descricao, $categoria_id, $usado)){ ?>
         <p class="text-success">Produto <?= $nome?>, <?= $preco?> adicionado com sucesso!.</p>
